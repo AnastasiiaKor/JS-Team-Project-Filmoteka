dfa0935bcec94566962a8245fb41cf3dee653b74
tsylepa <tsylepa@gmail.com>
2023-01-25 12:06:42 +0000

    try-catch refactoring

--- src/main.js/gallery.js
+++ src/main.js/gallery.js
@@ -2,10 +2,22 @@ import { getTrending } from './requests';
 
 const gallery = document.querySelector('.gallery');
 
-getTrending((page = 1)).then(function createMarkup({ results }) {
+buildGallery();
+
+async function buildGallery() {
+  try {
+    console.log('object');
+    const data = await getTrending((page = 1));
+    createMarkup(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function createMarkup({ results }) {
   const markup = results
     .map(({ id, poster_path, title, genre_ids, release_date }) => {
-      const date = new Date().getFullYear();
+      const date = new Date(release_date).getFullYear();
       let separator = '';
       if (genre_ids && date) separator = ' | ';
       return `<li class="gallery__item">
@@ -23,4 +35,4 @@ getTrending((page = 1)).then(function createMarkup({ results }) {
     .join('');
 
   gallery.insertAdjacentHTML('beforeend', markup);
-});
+}
