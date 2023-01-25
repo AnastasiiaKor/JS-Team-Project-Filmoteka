b074ce606bdc07b6477a240bc6374f1a415ae69c
ukrwebprom <ukrwebprom@gmail.com>
2023-01-25 23:23:22 +0200

    pagination.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
 import './main.js/requests';
-
-
-import { Paginator } from './main.js/paginator';
-
-const paginatorContainer = document.querySelector('.pagination');
-const p = new Paginator((page) => {
-    console.log(`Turn to page ${page}`)
-    p.currentPage = page;
-}, paginatorContainer);
-p.totalPages = 200;
-p.currentPage = 2;
-
 import './main.js/gallery';
+import './main.js/pagination';
 
