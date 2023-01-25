2c2b7bce0720573048a80079d75511061a3d5790
tsylepa <tsylepa@gmail.com>
2023-01-25 00:57:29 +0000

    gallery create

---
+++ src/main.js/gallery.js
@@ -0,0 +1,24 @@
+import { getTrending } from './requests';
+
+const gallery = document.querySelector('.gallery');
+
+getTrending((page = 1)).then(function createMarkup({ results }) {
+  const markup = results
+    .map(({ id, poster_path, title, genre_ids, release_date }) => {
+      const date = new Date(release_date).getFullYear();
+      return `<li class="gallery__item">
+                <a class="gallery__link" href="${id}">
+                    <img class="gallery__poster" src="https://image.tmdb.org/t/p/w500${poster_path}" loading="lazy" />
+                    <div class="gallery__description">
+                        <p class="gallery__movie-title">${title}</p>
+                        <p class="gallery__movie-meta">
+                        ${genre_ids} | ${date}
+                        </p>
+                    </div>
+                </a>
+            </li>`;
+    })
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+});
