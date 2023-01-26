f2844b69c61a4d66a556755f57ae2e82cfd14e65
tsylepa <tsylepa@gmail.com>
2023-01-26 17:42:21 +0000

    adds getGenresById function

--- src/main.js/templates.js/gallery-markup.js
+++ src/main.js/templates.js/gallery-markup.js
@@ -1,19 +1,14 @@
+import { getGenresById } from '../get-genres';
+
 function createGalleryMarkup(gallery, { results }) {
-  const genresList = JSON.parse(localStorage.getItem('genres'));
   const markup = results
     .map(
       ({ id, poster_path, title, genre_ids, release_date, vote_average }) => {
         const date = new Date(release_date).getFullYear();
         let separator = '';
         if (genre_ids && date) separator = ' | ';
-        let genres = genre_ids.map(
-          id => genresList.find(genre => genre.id === id).name
-        );
-        if (genres.length > 3) {
-          genres = genres.slice(0, 2);
-          genres.push('Other');
-        }
-        genres = genres.join(', ');
+        const genres = getGenresById(genre_ids);
+
         return `<li class="gallery__item">
                 <a class="gallery__link" href="${id}">
                     <div class='gallery__event-wrapper'>
@@ -21,10 +16,10 @@ function createGalleryMarkup(gallery, { results }) {
                         <div class="gallery__description">
                             <p class="gallery__movie-title">${title}</p>
                             <p class="gallery__movie-meta">
-                            ${genres}${separator}${date}
-                            <span class="gallery__movie-rating">${vote_average.toFixed(
-                              1
-                            )}</span>
+                              <span class="gallery__movie-info">${genres}${separator}${date}</span>
+                              <span class="gallery__movie-rating">${vote_average.toFixed(
+                                1
+                              )}</span>
                             </p>
                             
                         </div>
@@ -36,7 +31,7 @@ function createGalleryMarkup(gallery, { results }) {
     )
     .join('');
 
-  gallery.insertAdjacentHTML('beforeend', markup);
+  gallery.innerHTML = markup;
 }
 
 export { createGalleryMarkup };
