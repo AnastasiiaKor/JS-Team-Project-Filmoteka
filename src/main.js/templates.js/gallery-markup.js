215f443327ceeca1e966719d113862c134e5f00a
tsylepa <tsylepa@gmail.com>
2023-01-26 13:08:45 +0000

    add genres

--- src/main.js/templates.js/gallery-markup.js
+++ src/main.js/templates.js/gallery-markup.js
@@ -1,10 +1,20 @@
 function createGalleryMarkup(gallery, { results }) {
+  const genresList = JSON.parse(localStorage.getItem('genres'));
+  console.log(genresList);
   const markup = results
     .map(
       ({ id, poster_path, title, genre_ids, release_date, vote_average }) => {
         const date = new Date(release_date).getFullYear();
         let separator = '';
         if (genre_ids && date) separator = ' | ';
+        let genres = genre_ids.map(
+          id => genresList.find(genre => genre.id === id).name
+        );
+        if (genres.length > 3) {
+          genres = genres.slice(0, 2);
+          genres.push('Other');
+        }
+        genres = genres.join(', ');
         return `<li class="gallery__item">
                 <a class="gallery__link" href="${id}">
                     <div class='gallery__event-wrapper'>
@@ -12,7 +22,7 @@ function createGalleryMarkup(gallery, { results }) {
                         <div class="gallery__description">
                             <p class="gallery__movie-title">${title}</p>
                             <p class="gallery__movie-meta">
-                            ${genre_ids}${separator}${date}
+                            ${genres}${separator}${date}
                             <span class="gallery__movie-rating">${vote_average.toFixed(
                               1
                             )}</span>
