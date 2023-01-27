9c1c78affd9d44911183e672a2274fb159e9c978
Anastasiya Kormilitsina <anastasiya.kormilitsina@gmail.com>
2023-01-28 02:31:45 +0500

    fixing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import './main.js/requests';
-import './main.js/saveGenres';
-// import './main.js/trending';
-/* import './main.js/pagination'; */
-import './main.js/search';
+// import './main.js/requests';
+// import './main.js/saveGenres';
+// // import './main.js/trending';
+// /* import './main.js/pagination'; */
+// import './main.js/search';
 
-import './main.js/modal';
-import './main.js/login';
-import './main.js/theme-switcher';
+// import './main.js/modal';
+// import './main.js/login';
+// import './main.js/theme-switcher';
