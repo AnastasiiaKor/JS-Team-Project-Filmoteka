1a3ee907e7645e2fb512dfd486432b4d5a1270eb
Anastasiya Kormilitsina <anastasiya.kormilitsina@gmail.com>
2023-01-28 02:35:49 +0500

    fix the path

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './main.js/requests';
-import './main.js/saveGenres';
-import './main.js/trending';
+// import './main.js/saveGenres';
+// import './main.js/trending';
 
-import './main.js/search';
+// import './main.js/search';
 
-import './main.js/modal';
-import './main.js/login';
+// import './main.js/modal';
+// import './main.js/login';
 import './main.js/theme-switcher';
