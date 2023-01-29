cccdd492fac2a143356f5a843071be807622e6b1
Anastasiya Kormilitsina <anastasiya.kormilitsina@gmail.com>
2023-01-30 02:09:34 +0500

    fixes js modules

--- src/library.js
+++ src/library.js
@@ -3,3 +3,5 @@ import './main.js/local-storage';
 import './main.js/greetings';
 import './main.js/library-btn-logic';
 import './main.js/saveGenres';
+import './main.js/modal';
+import './main.js/footer-modal';
