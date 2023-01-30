72da4604afb271fe7e341afa2432ea014914e5db
AnastasiiaKor <109483200+AnastasiiaKor@users.noreply.github.com>
2023-01-30 19:07:05 +0000

    Deploying to gh-pages from @ AnastasiiaKor/JS-Team-Project-Filmoteka@3ae5ae5591c7ae97d65d7a9b283f67f5447ada3b 🚀

---
+++ index.7e497406.js
@@ -0,0 +1,2 @@
+window.onload=function(){document.querySelector("#preloader").classList.add("is-hidden")};
+//# sourceMappingURL=index.7e497406.js.map
---
+++ index.85602624.js
@@ -0,0 +1,2 @@
+window.onload=function(){document.querySelector("#preloader").classList.add("is-hidden")};
+//# sourceMappingURL=index.85602624.js.map
