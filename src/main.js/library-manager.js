2790ad64363710230c2fa0bda355bb83f0e64c81
ukrwebprom <ukrwebprom@gmail.com>
2023-01-30 18:15:11 +0200

    Update library-manager.js

--- src/main.js/library-manager.js
+++ src/main.js/library-manager.js
@@ -11,31 +11,37 @@ const isAlreadyQueued = (id) => {
 export const initBtns = (data) => {
     const btnAddToWatched = document.querySelector('[data-addto="watched"]');
     const btnAddToQueue = document.querySelector('[data-addto="queue"]');
+    data.genre_ids = data.genres.map(g => g.id);
 
     if(isAlreadyWatched(data.id)) {
         btnAddToWatched.textContent = 'remove from watched'
-        btnAddToWatched.addEventListener('click', async () => {
-           if(await removeMovie('watched', data.id)) initBtns(data);
-        })
     } else {
         btnAddToWatched.textContent = 'add to watched'
-        btnAddToWatched.addEventListener('click', async () => {
-            data.genre_ids = data.genres.map(g => g.id);
-            if(await setMovie('watched', data)) initBtns(data);
-        })
     };
 
     if(isAlreadyQueued(data.id)) {
         btnAddToQueue.textContent = 'remove from queue'
-        btnAddToQueue.addEventListener('click', async () => {
-           if(await removeMovie('queue', data.id)) initBtns(data);
-        })
     } else {
         btnAddToQueue.textContent = 'add to queue'
-        btnAddToQueue.addEventListener('click', async () => {
-            data.genre_ids = data.genres.map(g => g.id);
-            if(await setMovie('queue', data)) initBtns(data);
-        })
     };
     
+    btnAddToWatched.addEventListener('click', async () => {
+        if(isAlreadyWatched(data.id)) {
+            await removeMovie('watched', data.id);
+            btnAddToWatched.textContent = 'add to watched
+        } else {
+            await setMovie('watched', data);
+            btnAddToWatched.textContent = 'remove from watched'
+        }
+     })
+
+     btnAddToQueue.addEventListener('click', async () => {
+        if(isAlreadyQueued(data.id)) {
+            await removeMovie('queue', data.id);
+            btnAddToQueue.textContent = 'add to queue';
+        } else {
+            await setMovie('queue', data);
+            btnAddToQueue.textContent = 'remove from queue';
+        }
+    })
 }
