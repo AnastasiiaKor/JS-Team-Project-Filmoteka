e72bb93680235aa2527531905ba82658b7acb6fb
Anastasiya Kormilitsina <anastasiya.kormilitsina@gmail.com>
2023-01-31 04:26:50 +0500

    fix mistake

--- src/main.js/library-manager.js
+++ src/main.js/library-manager.js
@@ -1,55 +1,59 @@
 import { removeMovie, setMovie } from './firebase';
 import { USER } from './user-manager';
 
-const isAlreadyWatched = (id) => {
-    return JSON.parse(localStorage.getItem('watched')).map(e => e.id).includes(id);
-}
+const isAlreadyWatched = id => {
+  return JSON.parse(localStorage.getItem('watched'))
+    .map(e => e.id)
+    .includes(id);
+};
 
-const isAlreadyQueued = (id) => {
-    return JSON.parse(localStorage.getItem('queue')).map(e => e.id).includes(id);
-}
+const isAlreadyQueued = id => {
+  return JSON.parse(localStorage.getItem('queue'))
+    .map(e => e.id)
+    .includes(id);
+};
 
-export const initBtns = (data) => {
-    const btnAddToWatched = document.querySelector('[data-addto="watched"]');
-    const btnAddToQueue = document.querySelector('[data-addto="queue"]');
-    if(!USER) {
-        btnAddToWatched.disabled = true;
-        btnAddToQueue.disabled = true;
-        const message = document.querySelector('.nologin');
-        message.textContent = "Login is Required";
-        return;
+export const initBtns = data => {
+  const btnAddToWatched = document.querySelector('[data-addto="watched"]');
+  const btnAddToQueue = document.querySelector('[data-addto="queue"]');
+  if (!USER) {
+    btnAddToWatched.disabled = true;
+    btnAddToQueue.disabled = true;
+    const message = document.querySelector('.nologin');
+    message.textContent = 'Login is Required';
+    return;
+  }
+  data.genre_ids = data.genres.map(g => g.id);
+
+  if (isAlreadyWatched(data.id)) {
+    btnAddToWatched.textContent = 'remove from watched';
+  } else {
+    btnAddToWatched.textContent = 'add to watched';
+  }
+
+  if (isAlreadyQueued(data.id)) {
+    btnAddToQueue.textContent = 'remove from queue';
+  } else {
+    btnAddToQueue.textContent = 'add to queue';
+  }
+
+  btnAddToWatched.addEventListener('click', async () => {
+    if (isAlreadyWatched(data.id)) {
+      await removeMovie('watched', data.id);
+      btnAddToWatched.textContent = 'add to watched';
+    } else {
+      await setMovie('watched', data);
+      btnAddToWatched.textContent = 'remove from watched';
     }
-    data.genre_ids = data.genres.map(g => g.id);
+  });
 
-    if(isAlreadyWatched(data.id)) {
-        btnAddToWatched.textContent = 'remove from watched'
+  btnAddToQueue.addEventListener('click', async () => {
+    if (isAlreadyQueued(data.id)) {
+      await removeMovie('queue', data.id);
+      btnAddToQueue.textContent = 'add to queue';
     } else {
-        btnAddToWatched.textContent = 'add to watched'
-    };
-
-    if(isAlreadyQueued(data.id)) {
-        btnAddToQueue.textContent = 'remove from queue'
-    } else {
-        btnAddToQueue.textContent = 'add to queue'
-    };
-    
-    btnAddToWatched.addEventListener('click', async () => {
-        if(isAlreadyWatched(data.id)) {
-            await removeMovie('watched', data.id);
-            btnAddToWatched.textContent = 'add to watched
-        } else {
-            await setMovie('watched', data);
-            btnAddToWatched.textContent = 'remove from watched'
-        }
-     })
-
-     btnAddToQueue.addEventListener('click', async () => {
-        if(isAlreadyQueued(data.id)) {
-            await removeMovie('queue', data.id);
-            btnAddToQueue.textContent = 'add to queue';
-        } else {
-            await setMovie('queue', data);
-            btnAddToQueue.textContent = 'remove from queue';
-        }
-    })
-}
+      await setMovie('queue', data);
+      btnAddToQueue.textContent = 'remove from queue';
+    }
+  });
+};
