05a709beb57e8317ad9069e4b01ddd3490ec3888
Anastasiya Kormilitsina <anastasiya.kormilitsina@gmail.com>
2023-02-01 00:57:45 +0500

    sililar library

--- src/main.js/similar.js
+++ src/main.js/similar.js
@@ -4,6 +4,14 @@ import { paginator } from './paginator';
 let id;
 
 export const loadSimilar = reqID => {
+  if (location.pathname === '/library.html') {
+    const librWatchedBtnEl = document.querySelector('#watched');
+    const librQueueBtnEl = document.querySelector('#queue');
+    librWatchedBtnEl.classList.remove('btn-add__active');
+    librWatchedBtnEl.removeAttribute('disabled');
+    librQueueBtnEl.classList.remove('btn-add__active');
+    librQueueBtnEl.removeAttribute('disabled');
+  }
   id = reqID;
   paginator.callback = fetchSimilar;
   fetchSimilar(1);
