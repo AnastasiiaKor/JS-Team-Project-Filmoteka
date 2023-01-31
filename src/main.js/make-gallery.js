b4822c4b261ed6723de87c3842ff8ff41860a063
ukrwebprom <ukrwebprom@gmail.com>
2023-02-01 00:39:18 +0200

    scroll to top

--- src/main.js/make-gallery.js
+++ src/main.js/make-gallery.js
@@ -4,7 +4,8 @@ export const makeGallery = movies => {
     const gallery = document.querySelector('.gallery');
     const markup = createGalleryMarkup(movies);
     gallery.innerHTML = markup;
-    gallery.scrollIntoView();
+    /* gallery.scrollIntoView(); */
+    window.scrollTo(0, 0)
 }
 
 export const clearGallery = () => {
