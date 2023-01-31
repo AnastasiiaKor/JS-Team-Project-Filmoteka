cf9acfa9375dc50bab5b77d15ce24bae4354d0a1
Aleksey100ckiy <109811250+Aleksey100ckiy@users.noreply.github.com>
2023-01-31 23:37:06 +0200

    update save btn

--- src/main.js/library-btn-logic.js
+++ src/main.js/library-btn-logic.js
@@ -5,6 +5,7 @@ const divClassEl = document.querySelector('.empty_library_bcg_img');
 const librWatchedBtnEl = document.querySelector('#watched');
 const librQueueBtnEl = document.querySelector('#queue');
 const userName = localStorage.getItem('user');
+let defaultEvent = new Event('click');
 
 librWatchedBtnEl.addEventListener('click', onWatchedClick);
 librQueueBtnEl.addEventListener('click', onQueueClick);
@@ -14,6 +15,8 @@ function onWatchedClick(e) {
   librQueueBtnEl.removeAttribute('disabled');
   librWatchedBtnEl.classList.add('btn-add__active');
   librWatchedBtnEl.setAttribute('disabled', true);
+  localStorage.removeItem("saved_page");
+  localStorage.setItem("saved_page", "watched");
 
   renderMovies(e.target.id);
 }
@@ -23,6 +26,8 @@ function onQueueClick(e) {
   librWatchedBtnEl.removeAttribute('disabled');
   librQueueBtnEl.classList.add('btn-add__active');
   librQueueBtnEl.setAttribute('disabled', true);
+  localStorage.removeItem("saved_page");
+  localStorage.setItem("saved_page", "queue");
 
   renderMovies(e.target.id);
 }
@@ -51,3 +56,18 @@ document.addEventListener('libraryChange', e => {
 // подключить билдГаллери
 // подключить создатель разметки ок
 //добавить их в ф-цию onClick
+
+checkSaveBtn();
+function checkSaveBtn(){
+  
+  if(localStorage.getItem("saved_page") === "watched"){
+  
+    librWatchedBtnEl.dispatchEvent(defaultEvent);
+    return
+  } if(localStorage.getItem("saved_page") === "queue"){
+    librQueueBtnEl.dispatchEvent(defaultEvent);
+    return
+  }
+  else 
+  console.log('не робить');
+ }
