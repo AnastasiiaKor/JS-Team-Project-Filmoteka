aeba35832fff923b96a9cfc9eb4c15533b80cbdd
Kate Fanina <katherina.fanina@gmail.com>
2023-02-01 15:25:46 +0200

    add arial label attributes

--- src/main.js/greetings.js
+++ src/main.js/greetings.js
@@ -1,10 +1,7 @@
-/* const WAS_USER_GREETED = 'wasUserGreeted';
-const wasUserGreeted = localStorage.getItem(WAS_USER_GREETED);*/
 const welcome = document.getElementById('greatings');
 const user = localStorage.getItem('username');
   
-  if (welcome && user) {
-    welcome.innerHTML = `Welcome ${user}!`;
- 
-    localStorage.removeItem('username');
-  }
+if (welcome && user) {
+  welcome.innerHTML = `Welcome ${user}!`;
+  localStorage.removeItem('username');
+}
