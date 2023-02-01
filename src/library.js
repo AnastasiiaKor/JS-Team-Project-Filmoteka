98f8a0215f8e3bc017230fdf80832909fd5157c9
Anastasiya Kormilitsina <anastasiya.kormilitsina@gmail.com>
2023-02-02 00:24:27 +0500

    fix path

--- src/library.js
+++ src/library.js
@@ -3,7 +3,7 @@ import './main.js/theme-switcher';
 import './main.js/get-movies-by-genre';
 import './main.js/greetings';
 import './main.js/library-btn-logic';
-import './main.js/saveGenres';
+import './main.js/save-genres';
 import './main.js/modal';
 import './main.js/footer-modal';
 import './main.js/back-to-top';
