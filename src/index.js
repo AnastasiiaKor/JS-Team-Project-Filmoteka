e11c52f3f80fcda037f1eb89790e967cd10237d5
Anastasiya Kormilitsina <anastasiya.kormilitsina@gmail.com>
2023-02-02 01:58:51 +0500

    fixes

--- src/index.js
+++ src/index.js
@@ -5,10 +5,9 @@ import './main.js/trending';
 import './main.js/search';
 import './main.js/modal';
 import './main.js/get-movies-by-genre';
-/* import './main.js/login'; */
 import './main.js/user-manager';
 import './main.js/theme-switcher';
 import './main.js/local-storage';
 import './main.js/footer-modal';
 import './main.js/back-to-top';
-/* import './main.js/library-manager'; */
+import './main.js/templates.js/gallery-settigs';
--- src/library.js
+++ src/library.js
@@ -1,6 +1,6 @@
 import './main.js/preloader';
 import './main.js/theme-switcher';
-import './main.js/get-movies-by-genre';
+// import './main.js/get-movies-by-genre';
 import './main.js/greetings';
 import './main.js/library-btn-logic';
 import './main.js/save-genres';
